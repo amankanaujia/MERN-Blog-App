2a59c31f9131d6fbd2112f15c1e6add4b55b7ad0
amankanaujia <amankanaujia777@gmail.com>
2024-05-02 11:45:16 +0530

    url changes

--- server/index.js
+++ server/index.js
@@ -4,13 +4,12 @@ require("./db/index.js");
 const blogRouter = require("./route/blog-route.js");
 
 const app = express();
-app.use(
-  cors({
-    origin: [`https://mern-blog-app-ten.vercel.app`],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+// {
+//   origin: [`https://mern-blog-app-ten.vercel.app`],
+//   methods: ["POST", "GET"],
+//   credentials: true,
+// }
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
