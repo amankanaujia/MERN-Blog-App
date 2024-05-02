4baf05608996228799e3b3e61ef6678c58e78c6f
amankanaujia <amankanaujia777@gmail.com>
2024-05-02 13:11:15 +0530

    url changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [`https://mern-blog-app-eight.vercel.app`],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "UPDATE", "DELETE"],
     credentials: true,
   })
 );
